e97702507e484919155c66f10903e0eb77a676c6
bokob <bmc0520@naver.com>
2024-07-01 20:47:01 +0900

    [FIX] 호스트 방 떠날 시 모든 클라이언트 종료하게 하기 및 관련 .cs 파일에 주석 작성

--- Assets/Scripts/Lobby/ReadyUp.cs
+++ Assets/Scripts/Lobby/ReadyUp.cs
@@ -1,20 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
-using SlimUI.ModernMenu;
 using Unity.Netcode;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 
 public class ReadyUp : NetworkBehaviour
 {
-    UIMenuManager ui;
-    Dictionary<ulong, bool> playerReadyDictionary;
+    LobbyJoinedUI ui;
+    Dictionary<ulong, bool> playerReadyDictionary; // 각 클라이언트의 준비 상태를 저장하는 딕셔너리
 
-    public bool isLocalPlayerReady = false;
+    public bool isLocalPlayerReady = false;        // 로컬 플레이어(자기 자신)의 준비 상태를 나타낸다. 
     // Start is called before the first frame update
     private void Awake()
     {
         playerReadyDictionary = new Dictionary<ulong, bool>();
-        ui = GetComponent<UIMenuManager>();
+        ui = GetComponent<LobbyJoinedUI>();
     }
     public void SetLocalPlayerReady()
     {
@@ -24,11 +24,13 @@ public class ReadyUp : NetworkBehaviour
     }
 
     [ServerRpc(RequireOwnership = false)]
-    void SetPlayerReadyServerRpc(int _index, ServerRpcParams rpcParams = default) // server
+    void SetPlayerReadyServerRpc(int _index, ServerRpcParams rpcParams = default) // 서버에서 호출되는 RPC 메서드
     {
-        playerReadyDictionary[rpcParams.Receive.SenderClientId] = true;
+        // ServerRpcParams는 RPC를 호출한 클라이언트로부터 전달된 정보를 담고 있는 속성
 
-        SetPlayerReadyDisplayClientRpc(_index);
+        playerReadyDictionary[rpcParams.Receive.SenderClientId] = true; // Rpc를 호출한 클라이언트의 준비 상태를 true로 설정
+
+        SetPlayerReadyDisplayClientRpc(_index); // 다른 클라이언트에게 보여주기 위해 서버에서 실행한다. 그러면 다른 클라이언트에서 이 RPC 호출한다.
 
         bool allClientsReady = true;
         foreach (ulong clientId in NetworkManager.Singleton.ConnectedClientsIds)
@@ -41,7 +43,7 @@ public class ReadyUp : NetworkBehaviour
             }
         }
         Debug.Log("All clients ready: " + allClientsReady);
-        if (allClientsReady == true)
+        if (allClientsReady == true) // 모든 클라이언트가 준비 상태면, 게임씬 로드
         {
             NetworkGameManager.instance.LoadGameScene();
         }
@@ -69,13 +71,13 @@ public class ReadyUp : NetworkBehaviour
     }
 
     [ServerRpc(RequireOwnership = false)]
-    void SetPlayerUnreadyServerRpc(int _index, ServerRpcParams rpcParams = default)
+    void SetPlayerUnreadyServerRpc(int _index, ServerRpcParams rpcParams = default) // 서버에서 실행하는 RPC 메서드
     {
         playerReadyDictionary[rpcParams.Receive.SenderClientId] = false;
-        SetPlayerUnreadyDisplayClientRpc(_index);
+        SetPlayerUnreadyDisplayClientRpc(_index); // 다른 클라이언트도 반영할 수 있도록 클라이언트 RPC 호출
     }
     [ClientRpc]
-    void SetPlayerUnreadyDisplayClientRpc(int _index)
+    void SetPlayerUnreadyDisplayClientRpc(int _index) // 특정 플레이어의 준비 취소 상태를 UI에 표시
     {
         LobbyPlayer[] _players = FindObjectsOfType<LobbyPlayer>();
         foreach (LobbyPlayer player in _players)
