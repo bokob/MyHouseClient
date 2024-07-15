546a02cb53c089cbf27043cdda9d9115a74681be
bokob <bmc0520@naver.com>
2024-07-15 20:14:33 +0900

    [FIX] 카메라 컨트롤러로 카메라 관리

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,32 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Recorder.Input;
 using UnityEngine;
-<<<<<<< HEAD
-
-public class CameraController : MonoBehaviour
-{
-=======
 using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
-    // Camera Controller 임시
     [Header("Cinemachine")]
+    GameObject _mainCamera;                     // 메인 카메라
+    GameObject _quaterFollowCamera;
+    GameObject _thirdFollowCamera;
+    GameObject _aimCamera;
     public GameObject _cinemachineCameraTarget; // 카메라가 바라볼 목표물
     public float _topClamp = 70.0f;             // 카메라 위 제한 각도
     public float _bottomClamp = -30.0f;         // 카메라 아래 제한 각도
     public float _cameraAngleOverride = 0.0f;   // 카메라 회전 각도 미세 조정에 사용
     public bool _lockCameraPosition = false;    // 카메라 잠금
-    GameObject _mainCamera;                     // 메인 카메라
     float _cinemachineTargetYaw;                // 카메라 Y축 회전 제어 사용
     float _cinemachineTargetPitch;
+
+    public Define.View View = Define.View.None; // 카메라 시점
 
     // player
 #if ENABLE_INPUT_SYSTEM
     PlayerInput _playerInput;
 #endif
-
-    [SerializeField] public Define.Role PlayerRole { get; set; } = Define.Role.None;
 
     public PlayerInputs _input;
 
@@ -34,49 +32,68 @@ public class CameraController : MonoBehaviour
 
     public float _sensitivity = 1f;    
 
-    // 입력장치(키보드, 마우스 인식)
-    bool IsCurrentDeviceMouse
-    {
-        get
-        {
-#if ENABLE_INPUT_SYSTEM
-            return _playerInput.currentControlScheme == "KeyboardMouse";
-#else
-			return false;
-#endif
-        }
-    }
-
     // Start is called before the first frame update
     void Start()
     {
-        
+        CameraInit();
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        if(View != Define.View.Third)
+            CameraRotation();
     }
 
-    // 카메라 각도 제한
-    static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+
+    void CameraInit() // 카메라 초기 세팅
+    {
+        // 카메라 오브젝트 세팅
+        _mainCamera = gameObject;
+        _quaterFollowCamera = _mainCamera.transform.parent.GetChild(1).gameObject;
+        _thirdFollowCamera = _mainCamera.transform.parent.GetChild(2).gameObject;
+        _aimCamera = _mainCamera.transform.parent.GetChild(3).gameObject;
+    }
+
+    public void SetRobberView() // 강도 시점 설정
+    {
+        // 강도에 맞는 카메라 설정
+        _quaterFollowCamera.SetActive(true);
+        _thirdFollowCamera.SetActive(false);
+        _aimCamera.SetActive(false);
+    }
+
+    public void SetHouseownerView() // 집주인 시점 설정
+    {
+        // 집주인에 맞는 카메라 설정
+        _quaterFollowCamera.SetActive(false);
+        _thirdFollowCamera.SetActive(true);
+        _aimCamera.SetActive(true);
+    }
+
+    /// <summary>
+    /// 카메라 각도 제한
+    /// </summary>
+    /// <param name="lfAngle"></param>
+    /// <param name="lfMin"></param>
+    /// <param name="lfMax"></param>
+    /// <returns></returns>
+    static public float ClampAngle(float lfAngle, float lfMin, float lfMax)
     {
         if (lfAngle < -360f) lfAngle += 360f;
         if (lfAngle > 360f) lfAngle -= 360f;
         return Mathf.Clamp(lfAngle, lfMin, lfMax);
     }
 
-    // 카메라 회전
+    /// <summary>
+    /// 카메라 회전
+    /// </summary>
     void CameraRotation()
     {
-        if (PlayerRole == Define.Role.Robber) return;
-
         // if there is an input and camera position is not fixed
         if (_input.look.sqrMagnitude >= _threshold && !_lockCameraPosition)
         {
             //Don't multiply mouse input by Time.deltaTime;
-            float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
+            float deltaTimeMultiplier = 1.0f;
 
             // 정조준 할 때 천천히 돌아가야 하니까 Sensitivity를 넣어준다.
             _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier * _sensitivity;
