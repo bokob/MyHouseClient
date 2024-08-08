5a1d06fd686664543680f03efd2f5a27f4fdb407
dlqorns <snack121900@gmail.com>
2024-08-08 09:31:26 +0900

    [Fix] 충돌 해결을 위한 커밋

---
+++ Assets/Scripts/Single/PoolingManager_S.cs
@@ -0,0 +1,98 @@
+using Photon.Pun.Demo.PunBasics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+
+public class PoolingManager_S : MonoBehaviour
+{
+    public static PoolingManager_S _instance;
+
+    [SerializeField]
+    private GameObject _monsterPrefab;
+
+    private IObjectPool<ModifiedMonster_S> _pool;
+
+    public List<Transform> _waveSpawnPoints = new List<Transform>();
+    float spawnGhostInterval = 60f;  // Monster generation interval. Created after 60 seconds at first.
+    int additionalSpawnGhostCount = 0;  // Number of additional monsters to create.
+
+
+    private void Awake()
+    {
+        _instance = this;
+
+        if (_pool == null)
+        {
+            _pool = new ObjectPool<ModifiedMonster_S>(CreateMonster, OnGetMonster, OnReleaseMonster, maxSize: 100);
+        }
+    }
+
+    private void Start()
+    {
+        // Initially create two from each GhostWave object.
+        for (int i = 0; i < _waveSpawnPoints.Count; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                CreateMonsterAtSpawnPoint(i);
+            }
+        }
+
+        StartCoroutine(SpawnGhostsOverTime());
+    }
+
+    private IEnumerator SpawnGhostsOverTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnGhostInterval);
+
+            additionalSpawnGhostCount++;
+            spawnGhostInterval += 20f;
+
+            for (int i = 0; i < _waveSpawnPoints.Count; i++)
+            {
+                for (int j = 0; j < additionalSpawnGhostCount; j++)
+                {
+                    CreateMonsterAtSpawnPoint(i);
+                }
+            }
+        }
+    }
+
+    private void CreateMonsterAtSpawnPoint(int spawnPointIndex)
+    {
+        Vector3 randomPosition = _waveSpawnPoints[spawnPointIndex].position + Random.insideUnitSphere * 7f;
+        randomPosition.y = 0; // Fixed position.y value to 0 when creating Ghost.
+
+        ModifiedMonster_S monster = Instantiate(_monsterPrefab, randomPosition, Quaternion.identity, _waveSpawnPoints[spawnPointIndex]).GetComponent<ModifiedMonster_S>();
+        if (monster != null)
+        {
+            monster.SetManagedPool(_pool);
+        }
+        else
+        {
+            Debug.LogError("ModifiedMonster_S component not found");
+        }
+    }
+
+    private ModifiedMonster_S CreateMonster()
+    {
+        // this method is no longer used, but left to initialize the Pool.
+        return null;
+    }
+
+    private void OnGetMonster(ModifiedMonster_S monster)
+    {
+        monster.gameObject.SetActive(true);
+    }
+
+    private void OnReleaseMonster(ModifiedMonster_S monster)
+    {
+        if (monster.transform.position.y == -1.5f)
+        {
+            monster.gameObject.SetActive(false);
+        }
+    }
+}
