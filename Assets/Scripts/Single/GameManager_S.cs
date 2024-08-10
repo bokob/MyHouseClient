cdc3e869a81270720fd93d5a4b03a223f27fdc03
Cho Min Gyu <jmk02355@gmail.com>
2024-08-10 18:31:20 +0900

    [FIX] 무기 줍기 및 json 수정

--- Assets/Scripts/Single/GameManager_S.cs
+++ Assets/Scripts/Single/GameManager_S.cs
@@ -8,12 +8,6 @@ public class GameManager_S : MonoBehaviour
 {    
     public static GameManager_S _instance;
     public event EventHandler OnVariableChanged;
-
-    public WeaponManager_S weaponManager_S;
-    public string _currentWeapon;
-    public int _currentAttack;
-    public float _currentRate;
-
     public int _monsterCount = 0;
     public int _score = 0;
     public int Score
@@ -29,23 +23,6 @@ public class GameManager_S : MonoBehaviour
     {
         _instance = this;
         _score = 0;
-    }
-
-    // 무기를 주웠을 때 호출되는 메서드
-    public void PickUpWeapon(string weaponName)
-    {
-        WeaponData weapon = weaponManager_S.GetWeaponByName(weaponName);
-        if (weapon != null)
-        {
-            Debug.Log($"Picked up {weapon.Name}. Attack: {weapon.Attack}, Rate: {weapon.Rate}");
-            _currentWeapon = weapon.Name;
-            _currentAttack = weapon.Attack;
-            _currentRate = weapon.Rate;
-        }
-        else
-        {
-            Debug.LogWarning("Weapon not found!");
-        }
     }
 }
 
