0ae5a4d39b24f1cd993bba0367189a85a82310cb
bokob <bmc0520@naver.com>
2024-08-22 19:55:16 +0900

    [FIX] Monster도 IStatus 상속받아서 한 번에 처리

--- Assets/Scripts/Common/Player/IStatus.cs
+++ Assets/Scripts/Common/Player/IStatus.cs
@@ -1,11 +1,19 @@
+using Photon.Pun;
+using System;
+
 public interface IStatus
 {
     #region 상태 및 능력치
-    Define.Role Role { get; set; }
+    public Define.Role Role { get;  set; }
     public float Hp { get; set; }
     public float Sp { get; set; }
     public float MaxHp { get; set; }
     public float MaxSp { get; set; }
     public float Defence { get; set; }
     #endregion
+
+
+    public void TakedDamage(int attack, PhotonMessageInfo info) { }
+    public void TakedDamage(int attack) { }
+    public void HitChangeMaterials() { }
 }
