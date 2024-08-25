11243514252142cfca8d39bb56dc19bf738fe008
bokob <bmc0520@naver.com>
2024-08-25 23:56:40 +0900

    [FIX] 싱글 미니맵 전체 버전 및 층별 활성화

--- Assets/Scripts/Common/MinimapCameraPosition.cs
+++ Assets/Scripts/Common/MinimapCameraPosition.cs
@@ -4,18 +4,45 @@ using UnityEngine;
 
 public class MinimapCameraPosition : MonoBehaviour
 {
-    [SerializeField]
-    bool x, y, z;
-    [SerializeField]
+    Camera minimapCamera;
+    [SerializeField] bool x, y, z;
+    [SerializeField] GameObject player;
     Transform target;
 
+    [SerializeField]  int secondFloorLayer;
+
+    private void Start()
+    {
+        minimapCamera = GetComponent<Camera>();
+
+        // 플레이어
+        player = transform.root.GetChild(1).gameObject;
+
+        // 미니맵 위치 설정
+        target = transform;
+        target.position = new Vector3(0, 0, 0);
+        target.SetPositionAndRotation(new Vector3(0,30,0), Quaternion.Euler(90,0,0));
+
+        secondFloorLayer = LayerMask.NameToLayer("Floor");
+    }
     private void Update()
     {
         if(!target) return;
 
+        // 미니맵 위치 고정 (플레이어의 자식에 카메라가 있으므로)
         transform.position = new Vector3(
-            (x ? target.position.x : transform.position.x),
-            (y ? target.position.y : transform.position.y),
-            (z ? target.position.z : transform.position.z));
+            (transform.position.x),
+            (transform.position.y),
+            (transform.position.z));
+
+        // 2층 렌더링 여부
+        if (player.transform.position.y > 10)
+        {
+            minimapCamera.cullingMask |= (1 << secondFloorLayer);
+        }
+        else
+        {
+            minimapCamera.cullingMask &= ~(1 << secondFloorLayer);
+        }
     }
 }
