a2c860e681b2e12a440c0d9f1e5b40db76c82389
bokob <bmc0520@naver.com>
2024-10-06 04:01:30 +0900

    [FIX] 플레이어 애니메이션 trigger -> bool로 변경

--- Assets/Scripts/Multi/Weapon/Melee.cs
+++ Assets/Scripts/Multi/Weapon/Melee.cs
@@ -23,14 +23,7 @@ public class Melee : Weapon
     bool _isStabReady;  // 공격 준비
     float _stabDelay;   // 공격 딜레이
 
-    #region 절단 효과 변수
-    public LayerMask _sliceMask; // 자를 대상인 레이어 마스크
-    public float _cutForce = 250f; // 자를 때 가해지는 힘
-
-    Vector3 _entryPoint; // 오브젝트에 들어간 지점
-    Vector3 _exitPoint; // 오브젝트를 뚫고 나간 지점
-    bool _hasExited = false; // 오브젝트를 뚫고 나갔는지 여부를 저장하는 변수
-    #endregion
+    PhotonView PV;
 
     private void Awake()
     {
@@ -49,6 +42,7 @@ public class Melee : Weapon
         else if (_playerStatus.Role == Define.Role.None)
             Debug.Log("왜 None이야?");
 
+        PV = GetComponent<PhotonView>();
     }
 
     void InitWeapon()
@@ -111,14 +105,16 @@ public class Melee : Weapon
             {
                 Debug.Log("휘두르기");
                 // _weaponManager._selectedWeapon.GetComponent<Melee>().Use();
-                _animator.SetTrigger("setSwing");
+                //_animator.SetTrigger("setSwing");
+                _animator.SetBool("isSwing", true);
                 _swingDelay = 0;
             }
             else if (_playerInputs.stab && _playerMove._grounded) // 찌르기
             {
                 Debug.Log("찌르기");
                 // _weaponManager._selectedWeapon.GetComponent<Melee>().Use();
-                _animator.SetTrigger("setStab");
+                //_animator.SetTrigger("setStab");
+                _animator.SetBool("isStab", true);
                 _stabDelay = 0;
 
             }
@@ -140,20 +136,22 @@ public class Melee : Weapon
     IEnumerator MeleeAttackEffect()
     {
         yield return new WaitForSeconds(0.5f);
-        GetComponent<PhotonView>().RPC("SetMeleeArea", RpcTarget.All, true);
-        GetComponent<PhotonView>().RPC("SetTrailEffect", RpcTarget.All, true);
+        PV.RPC("SetMeleeArea", RpcTarget.All, true);
+        PV.RPC("SetTrailEffect", RpcTarget.All, true);
 
         yield return new WaitForSeconds(0.5f);
-        GetComponent<PhotonView>().RPC("SetMeleeArea", RpcTarget.All, false);
+        PV.RPC("SetMeleeArea", RpcTarget.All, false);
 
         yield return new WaitForSeconds(0.5f);
-        GetComponent<PhotonView>().RPC("SetTrailEffect", RpcTarget.All, false);
+        PV.RPC("SetTrailEffect", RpcTarget.All, false);
     }
 
     // punchCollider 상태를 모든 클라이언트에서 설정하는 ClientRpc 메서드
     [PunRPC]
     void SetMeleeArea(bool state)
     {
+        _animator.SetBool("isSwing", false);
+        _animator.SetBool("isStab", false);
         _meleeArea.enabled = state;
     }
 
@@ -164,16 +162,8 @@ public class Melee : Weapon
         _trailEffet.enabled = state;
     }
 
-    #region 절단 기능
-    // 칼이 트리거 안에 있을 때
-    // _hasExited를 false로 설정
     void OnTriggerEnter(Collider other)
     {
-        _hasExited = false;
-        _entryPoint = other.ClosestPoint(transform.position);
-
-        // 데미지 적용
-
         // 자기 자신에게 닿은 경우 무시
         if (other.transform.root.name.Contains("Player") && other.transform.root.GetChild(2).GetComponent<PlayerStatus>() == _playerStatus)
         {
@@ -193,75 +183,4 @@ public class Melee : Weapon
             monster.GetComponent<IStatus>().TakedDamage(Attack);
         }
     }
-
-    void OnTriggerStay(Collider other)
-    {
-        Debug.Log("관통");
-    }
-
-    // 관통 다 되면 레이어에 따라 절단
-    void OnTriggerExit(Collider other)
-    {
-        MeshCutting(other);
-    }
-
-    // 메시 자르기
-    public void MeshCutting(Collider other)
-    {
-        // 충돌 지점의 방향을 자르는 방향으로 설정
-        _exitPoint = other.ClosestPoint(transform.position);
-
-        Vector3 cutDirection = _exitPoint - _entryPoint;
-        Vector3 cutInPlane = (_entryPoint + _exitPoint) / 2;
-
-        //Vector3 cutPlaneNormal = Vector3.Cross((entryPoint - exitPoint), (entryPoint - transform.position)).normalized;
-        Vector3 cutPlaneNormal = Vector3.Cross((_entryPoint - _exitPoint), (_entryPoint - transform.position)).normalized;
-        Debug.Log(cutPlaneNormal.x + ", " + cutPlaneNormal.y + ", " + cutPlaneNormal.z);
-
-        if (cutPlaneNormal.x == 0 && cutPlaneNormal.y == 0 && cutPlaneNormal.z == 0)
-        {
-            // 원래 자르던 방향을 normalize 해서 넣어줘야 됨
-            cutPlaneNormal = (_entryPoint - _exitPoint).normalized;
-            Debug.Log("대체: " + cutPlaneNormal.x + " " + cutPlaneNormal.y + " " + cutPlaneNormal.z);
-
-            bool isHorizontalCut = Mathf.Abs(cutDirection.x) > Mathf.Abs(cutDirection.y);
-
-            // 가로로 자르는 경우
-            if (isHorizontalCut)
-            {
-                // x 축 방향으로 자르기 때문에 cutPlaneNormal을 x 축 방향 벡터로 설정
-                cutPlaneNormal = Vector3.up;
-            }
-            else // 세로로 자르는 경우
-            {
-                // y 축 방향으로 자르기 때문에 cutPlaneNormal을 y 축 방향 벡터로 설정
-                cutPlaneNormal = Vector3.right;
-            }
-        }
-
-        LayerMask cutableMask = LayerMask.GetMask(LayerMask.LayerToName(other.gameObject.layer));
-        //Debug.Log("잘릴 레이어: " + LayerMask.LayerToName(other.gameObject.layer));
-        if (_sliceMask.value == cutableMask)
-        {
-            Debug.LogWarning("자를 수 있는 오브젝트");
-            // 오브젝트를 자르기
-            Cutter.Cut(other.gameObject, cutInPlane, cutPlaneNormal);
-
-            // 자를 때 가해지는 힘을 적용하여 오브젝트를 밀어냄
-            Rigidbody rb = other.GetComponent<Rigidbody>();
-            if (rb != null)
-            {
-                rb.AddForce(-cutPlaneNormal * _cutForce); // cutDirection 대신에 cutPlaneNormal을 사용
-            }
-
-            _hasExited = true;
-        }
-        else
-        {
-            //Debug.Log("sliceMask: " + sliceMask.value);
-            //Debug.Log("자를 레이어: " + other.gameObject.layer);
-            Debug.LogWarning("왜 안돼?");
-        }
-    }
-    #endregion
 }
